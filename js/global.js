fa9f012a10dfa9abb588968a0d9600055ce8bb25
xxnbeatxx <derpyminesheeps@gmail.com>
2014-05-15 09:38:06 -0600

    alsdkfj
    sdfddfdffdfdfgjjjfjjdfjkdfjk

---
+++ js/global.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+function save()
+		{
+			var hole1 = document.getElementById("hole1").value;
+			var hole2 = document.getElementById("hole2").value;
+			var hole3 = document.getElementById("hole3").value;
+			var hole4 = document.getElementById("hole4").value;
+			var hole5 = document.getElementById("hole5").value;
+			var hole6 = document.getElementById("hole6").value;
+			var hole7 = document.getElementById("hole7").value;
+			var hole8 = document.getElementById("hole8").value;
+			var hole9 = document.getElementById("hole9").value;
+			var hole10 = document.getElementById("hole10").value;
+			var hole11 = document.getElementById("hole11").value;
+			var hole12 = document.getElementById("hole12").value;
+			var hole13 = document.getElementById("hole13").value;
+			var hole14 = document.getElementById("hole14").value;
+			var hole15 = document.getElementById("hole15").value;
+			var hole16 = document.getElementById("hole16").value;
+			var hole17 = document.getElementById("hole17").value;
+			var hole18 = document.getElementById("hole18").value;
+		function view_score()
+		{
+			
+		}
